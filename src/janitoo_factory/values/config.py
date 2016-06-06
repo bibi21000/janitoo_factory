53cb95c0e52f3e8669fb2113684ebfa35f06a4b1
bibi21000 <bibi21000@gmail.com>
2016-06-06 11:00:54 +0200

    Add new values

--- src/janitoo_factory/values/config.py
+++ src/janitoo_factory/values/config.py
@@ -251,7 +251,7 @@ class JNTValueConfigArray(JNTValueConfigGeneric):
         help = kwargs.pop('help', 'An array of strings separated by |')
         label = kwargs.pop('label', 'Array')
         get_data_cb = kwargs.pop('get_data_cb', self._get_data_list)
-        set_data_cb = kwargs.pop('set_data_cb', self._set_data_list)
+        #~ set_data_cb = kwargs.pop('set_data_cb', self._set_data_list)
         JNTValueConfigGeneric.__init__(self, entry_name=entry_name, help=help, label=label,
             get_data_cb=get_data_cb,
             #~ set_data_cb=set_data_cb,
